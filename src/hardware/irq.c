8c8c66b9f3ba64440cac5962db01be2f4612ae1f
Rhys Baker <bakerrhys05@gmail.com>
2024-08-25 21:57:03 +0800

    move functions around to avoid implicit declarations

--- src/hardware/irq.c
+++ src/hardware/irq.c
@@ -9,29 +9,14 @@
 
 bool vblank = false;
 
-void initIRQ(){
-    installExceptionHandler();
-   setInterruptHandler(interruptHandlerFunction, NULL);
-   IRQ_MASK = (1 << IRQ_VSYNC) | (1 << IRQ_CDROM);
-   enableInterrupts();
-}
-
+// Sets the global vblank variable to true.
 void handleVSyncIRQ(void){
-   vblank = true;
+    vblank = true;
 }
-
-void interruptHandlerFunction(void *arg) {
-   if(acknowledgeInterrupt(IRQ_VSYNC)){
-      handleVSyncIRQ();
-   }
-   if(acknowledgeInterrupt(IRQ_CDROM)){
-      handleCDROMIRQ();
-   }
-}
-
 
 void handleCDROMIRQ(void) {
     CDROM_ADDRESS = 1;
+
     uint8_t irqType = CDROM_HINTSTS & (0
         | CDROM_HINT_INT0
         | CDROM_HINT_INT1
@@ -74,4 +59,25 @@ void handleCDROMIRQ(void) {
             cdromINT5();
             break;
     }
+}
+
+// This is the first step to handling the IRQ.
+// It will acknowledge the interrupt on the COP0 side, and call the relevant handler for the device.
+void interruptHandlerFunction(void *arg) {
+    if(acknowledgeInterrupt(IRQ_VSYNC)){
+        handleVSyncIRQ();
+    }
+    if(acknowledgeInterrupt(IRQ_CDROM)){
+        handleCDROMIRQ();
+    }
+}
+
+void initIRQ(){
+    installExceptionHandler();
+    // This is the function that is called when an interrupt is raised.
+    // You can also pass an argument to this handler.
+    setInterruptHandler(interruptHandlerFunction, NULL);
+    // The IRQ mask specifies which interrupt sources are actually allowed to raise an interrupt.
+    IRQ_MASK = (1 << IRQ_VSYNC) | (1 << IRQ_CDROM);
+    enableInterrupts();
 }
