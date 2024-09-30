d2d1ac3b680ad85256a0474d348294c555efb749
Rhys Baker <bakerrhys05@gmail.com>
2024-10-01 03:52:56 +0800

    Port SpicyJpeg's SPU code from C++ to C

--- include/hardware/spu.h
+++ include/hardware/spu.h
@@ -1,3 +1,158 @@
+// Adapted by Rhys Baker
+// Based on the C++ code at https://github.com/spicyjpeg/573in1/blob/dev/src/common/spu.hpp
+
 #pragma once
 
+#include <stddef.h>
+#include <stdint.h>
+#include "registers.h"
+
+
+#define Channel int
+#define ChannelMask uint32_t
+
+enum LoopFlag {
+    LOOP_END     = 1 << 0,
+    LOOP_SUSTAIN = 1 << 1,
+    LOOP_START   = 1 << 2
+};
+
+static const uint32_t DUMMY_BLOCK_OFFSET = 0x01000;
+static const uint32_t DUMMY_BLOCK_END    = 0x01010;
+static const uint32_t SPU_RAM_END        = 0x7fff0;
+
+static const int      NUM_CHANNELS = 24;
+static const uint16_t MAX_VOLUME   = 0x3fff;
+
+static const ChannelMask ALL_CHANNELS = (1 << NUM_CHANNELS) - 1;
+
+/* Utilities */
+
+static inline uint32_t concat4(uint8_t a, uint8_t b, uint8_t c, uint8_t d){
+    return (a | (b << 8) | (c << 16) | (d << 24));
+}
+
+static inline int min(int a, int b){
+    return (a<b) ? a : b;
+}
+
+/* Basic SPU API */
+
 void initSPU(void);
+Channel getFreeChannel(void);
+ChannelMask getFreeChannels(int count);
+void stopChannels(ChannelMask mask);
+
+static inline void setMasterVolume(uint16_t master, uint16_t reverb){
+    SPU_MASTER_VOL_L = master;
+    SPU_MASTER_VOL_R = master;
+    SPU_REVERB_VOL_L = reverb;
+    SPU_REVERB_VOL_R = reverb;
+}
+
+static inline void stopChannel(Channel ch){
+    stopChannels(1 << ch);
+}
+
+size_t upload(uint32_t offset, const void *data, size_t length, bool wait);
+size_t download(uint32_t offset, void *data, size_t length, bool wait);
+
+
+/* VAGHeader Class */
+
+static const size_t INTERLEAVED_VAG_BODY_OFFSET = 2048;
+
+
+typedef struct VAGHeader{
+    uint32_t magic, version, interleave, length, sampleRate;
+    uint16_t _reserved[5], channels;
+    char     name[16];
+} VAGHeader;
+
+inline bool vagHeader_validateMagic(VAGHeader *vagHeader){
+    return (vagHeader->magic == concat4('V', 'A', 'G', 'p')) && (vagHeader->channels <= 1);
+}
+
+inline bool vagHeader_validateInterleavedMagic(VAGHeader *vagHeader){
+    return (vagHeader->magic == concat4('V', 'A', 'G', 'i')) && vagHeader->interleave;
+}
+
+inline uint16_t vagHeader_getSPUSampleRate(VAGHeader *vagHeader){
+    return(__builtin_bswap32(vagHeader->sampleRate) << 12) / 44100;
+}
+
+inline uint16_t vagHeader_getSPUSampleRate(VAGHeader *vagHeader){
+    return __builtin_bswap32(vagHeader->length) /8;
+}
+
+inline int getNumChannels(VAGHeader *vagHeader){
+    return vagHeader->channels ? vagHeader->channels : 2;
+}
+
+inline const void *getData(VAGHeader *vagHeader){
+    return vagHeader + 1;
+}
+
+
+/* Sound Class */
+
+typedef struct Sound {
+    uint32_t offset;
+    uint16_t sampleRate, length;
+} Sound;
+
+bool sound_initFromVAGHeader(Sound *sound, const VAGHeader *vagHeader, uint32_t _offset);
+Channel sound_playOnChannel(Sound *sound, uint16_t left, uint16_t right, Channel ch);
+
+inline Channel sound_play(Sound *sound, uint16_t left, uint16_t right){
+    return sound_playOnChannel(sound, left, right, getFreeChannel());
+}
+
+
+/* Stream Class */
+
+typedef struct Stream{
+    uint32_t _channelMask;
+    uint16_t _head, _tail, _bufferedChunks;
+
+    uint32_t offset;
+    uint16_t interleave, numChunks, sampleRate, channels;
+} Stream;
+
+inline uint32_t stream_getChunkOffset(Stream *stream, size_t chunk) {
+    return stream->offset + stream_getChunkLnegth(stream) * chunk;
+}
+void stream_configureIRQ(Stream stream);
+
+// Destructor here if needed
+
+inline ChannelMask stream_start(Stream *stream, uint16_t left, uint16_t right){
+    return stream_startWithChannelMask(stream, left, right, getFreeChannels(NUM_CHANNELS));
+}
+inline bool stream_isPlaying(Stream *stream){
+    __atomic_signal_fence(__ATOMIC_ACQUIRE);
+
+    return (stream->_channelMask != 0);
+}
+inline bool stream_isUnderrun(Stream *stream){
+    __atomic_signal_fence(__ATOMIC_ACQUIRE);
+
+    return !stream->_bufferedChunks;
+}
+
+inline size_t stream_getChunkLength(Stream *stream) {
+    __atomic_signal_fence(__ATOMIC_ACQUIRE);
+
+    // The currently playing chunk cannot be overwritten.
+    size_t playingChunk = stream->_channelMask ? 1 : 0;
+    return stream->numChunks - (stream->_bufferedChunks + playingChunk);
+}
+
+Stream *stream_create(void);
+bool stream_initFromVAGHeader(Stream *stream, const VAGHeader *vagHeader, uint32_t _offset, size_t _numChunks);
+ChannelMask stream_startWithChannelMask(Stream *stream, uint16_t left, uint16_t right, ChannelMask mask);
+void stream_stop(Stream *stream);
+void stream_handleInterrupt(Stream *stream);
+
+size_t stream_feed(Stream *stream, const void *data, size_t length);
+void stream_resetBuffer(Stream *stream);
--- src/hardware/spu.c
+++ src/hardware/spu.c
@@ -1,12 +1,419 @@
+#include <assert.h>
+#include <stdint.h>
 #include "spu.h"
-#include "ps1/registers.h"
+#include "registers.h"
+#include "system.h"
 
-void initSPU(void) {
-    BIU_DEV4_CTRL = 0x200931e1; // Configure bus
+/* Basic API */
 
-    SPU_CTRL = SPU_CTRL_CDDA | SPU_CTRL_UNMUTE | SPU_CTRL_ENABLE;
-    SPU_MASTER_VOL_L = 0x3fff;
-    SPU_MASTER_VOL_R = 0x3fff;
-    SPU_CDDA_VOL_L = 0x7fff;
-    SPU_CDDA_VOL_R = 0x7fff;
+static const int _DMA_CHUNK_SIZE = 4;
+static const int _DMA_TIMEOUT    = 100000;
+static const int _STATUS_TIMEOUT = 10000;
+
+static bool _waitForStatus(uint16_t mask, uint16_t value){
+    for(int timeout = _STATUS_TIMEOUT; timeout > 0; timeout -= 10) {
+        if((SPU_STAT & mask) == value){
+            return true;
+        }
+
+        delayMicroseconds(10);
+    }
+
+    return false;
+}
+
+void initSPU(void){
+    BIU_DEV4_CTRL = 0
+        | ( 1 <<  0) // Write delay
+        | (14 <<  4) // Read delay
+        | BIU_CTRL_RECOVERY
+        | BIU_CTRL_WIDTH_16
+        | BIU_CTRL_AUTO_INCR
+        | ( 9 << 16) // Number of address lines
+        | ( 0 << 24) // DMA read/write delay
+        | BIU_CTRL_DMA_DELAY;
+    
+    SPU_CTRL = 0;
+    _waitForStatus(0x3f, 0);
+
+    SPU_MASTER_VOL_L = 0;
+    SPU_MASTER_VOL_R = 0;
+    SPU_REVERB_VOL_L = 0;
+    SPU_REVERB_VOL_R = 0;
+    SPU_REVERB_ADDR  = SPU_RAM_END / 8;
+
+    SPU_FLAG_FM1     = 0;
+    SPU_FLAG_FM2     = 0;
+    SPU_FLAG_NOISE1  = 0;
+    SPU_FLAG_NOISE2  = 0;
+    SPU_FLAG_REVERB1 = 0;
+    SPU_FLAG_REVERB2 = 0;
+
+    SPU_CTRL = SPU_CTRL_ENABLE;
+    _waitForStatus(0x3f, 0);
+
+    // Place a dummy (silent) looping block at the beginning of SPU RAM.
+    SPU_DMA_CTRL = 4;
+    SPU_ADDR     = DUMMY_BLOCK_OFFSET / 8;
+
+    SPU_DATA = 0x0500;
+    for(int i = 7; i > 0; i--){
+        SPU_DATA = 0;
+    }
+
+    SPU_CTRL = SPU_CTRL_XFER_WRITE | SPU_CTRL_ENABLE;
+    _waitForStatus(SPU_CTRL_XFER_BITMASK | SPU_STAT_BUSY, SPU_CTRL_XFER_WRITE);
+    delayMicroseconds(100);
+
+    SPU_CTRL = SPU_CTRL_UNMUTE | SPU_CTRL_ENABLE;
+    stopChannel(ALL_CHANNELS);
+}
+
+Channel getFreeChannel(void) {
+    disableInterrupts();
+
+    for(Channel ch = 0; ch < NUM_CHANNELS; ch++){
+        if(!SPU_CH_ADSR_VOL(ch)){
+            enableInterrupts();
+            return ch;
+        }
+    }
+    enableInterrupts();
+    return -1;
+}
+
+ChannelMask getFreeChannels(int count){
+    disableInterrupts();
+
+    ChannelMask mask = 0;
+
+    for(Channel ch = 0; ch < NUM_CHANNELS; ch++){
+        if(SPU_CH_ADSR_VOL(ch)){
+            continue;
+        }
+
+        mask |= 1 << ch;
+        count--;
+
+        if(!count){
+            enableInterrupts();
+            return mask;
+        }
+    }
+    enableInterrupts();
+    return 0;
+}
+
+void stopChannels(ChannelMask mask){
+    mask &= ALL_CHANNELS;
+
+    SPU_FLAG_OFF1 = mask & 0xffff;
+    SPU_FLAG_OFF2 = mask >> 16;
+
+    for(Channel ch = 0; mask; ch++, mask >>= 1){
+        if(!(mask & 1)){
+            continue;
+        }
+
+        SPU_CH_VOL_L(ch) = 0;
+        SPU_CH_VOL_R(ch) = 0;
+        SPU_CH_FREQ(ch)  = 1 << 12;
+        SPU_CH_ADDR(ch)  = DUMMY_BLOCK_OFFSET / 8;
+    }
+
+    SPU_FLAG_ON1 = mask & 0xffff;
+    SPU_FLAG_ON2 >> 16;
+}
+
+size_t upload(uint32_t offset, const void *data, size_t length, bool wait){
+    length /= 4;
+    
+    // (Assert data is aligned uint32_t)
+
+    length = (length + _DMA_CHUNK_SIZE - 1) / _DMA_CHUNK_SIZE;
+
+    if(!waitForDMATransfer(DMA_SPU, _DMA_TIMEOUT)){
+        return 0;
+    }
+
+    uint16_t ctrlReg = SPU_CTRL & ~SPU_CTRL_XFER_BITMASK;
+
+    SPU_CTRL = ctrlReg;
+    _waitForStatus(SPU_CTRL_XFER_BITMASK, 0);
+
+    SPU_DMA_CTRL = 4;
+    SPU_ADDR     = offset / 8;
+    SPU_CTRL     = ctrlReg | SPU_CTRL_XFER_DMA_WRITE;
+    _waitForStatus(SPU_CTRL_XFER_BITMASK, SPU_CTRL_XFER_DMA_WRITE);
+
+    DMA_MADR(DMA_SPU) = (uint32_t)(data);
+    DMA_BCR (DMA_SPU) = concat4(_DMA_CHUNK_SIZE, length, 0, 0);
+    DMA_CHCR(DMA_SPU) = 0
+        | DMA_CHCR_WRITE
+        | DMA_CHCR_MODE_SLICE
+        | DMA_CHCR_ENABLE;
+    
+    if(wait){
+        waitForDMATransfer(DMA_SPU, _DMA_TIMEOUT);
+    }
+
+    return length * _DMA_CHUNK_SIZE * 4;
+}
+
+size_t download(uint32_t offset, void * data, size_t length, bool wait){
+     length /= 4;
+    
+    // (Assert data is aligned uint32_t)
+
+    length = (length + _DMA_CHUNK_SIZE - 1) / _DMA_CHUNK_SIZE;
+
+    if(!waitForDMATransfer(DMA_SPU, _DMA_TIMEOUT)){
+        return 0;
+    }
+
+    uint16_t ctrlReg = SPU_CTRL & ~SPU_CTRL_XFER_BITMASK;
+
+    SPU_CTRL = ctrlReg;
+    _waitForStatus(SPU_CTRL_XFER_BITMASK, 0);
+
+    SPU_DMA_CTRL = 4;
+    SPU_ADDR     = offset / 8;
+    SPU_CTRL     = ctrlReg | SPU_CTRL_XFER_DMA_READ;
+    _waitForStatus(SPU_CTRL_XFER_BITMASK, SPU_CTRL_XFER_DMA_READ);
+
+    DMA_MADR(DMA_SPU) = (uint32_t)(data);
+    DMA_BCR (DMA_SPU) = concat4(_DMA_CHUNK_SIZE, length, 0, 0);
+    DMA_CHCR(DMA_SPU) = 0
+        | DMA_CHCR_READ
+        | DMA_CHCR_MODE_SLICE
+        | DMA_CHCR_ENABLE;
+    
+    if(wait){
+        waitForDMATransfer(DMA_SPU, _DMA_TIMEOUT);
+    }
+
+    return length * _DMA_CHUNK_SIZE * 4;
+}
+
+/* Sound Class */
+Sound *sound_create(void){
+    Sound sound = {
+        .offset     = 0,
+        .sampleRate = 0,
+        .length     = 0
+    };
+    return &sound;
+}
+
+bool sound_initFromVAGHeader(Sound *sound, const VAGHeader *vagHeader, uint32_t _offset){
+    if(!vagHeader_validateMagic(vagHeader)){
+        return false;
+    }
+    sound->offset     = _offset;
+    sound->sampleRate = vagHeader_getSPUSampleRate(vagHeader);
+    sound->length     = vagHeader_getSPULength(vagHeader);
+    return true;
+}
+
+Channel sound_playOnChannel(Sound *sound, uint16_t left, uint16_t right, Channel ch) {
+    if((ch<0) || (ch >= NUM_CHANNELS)){
+        return -1;
+    }
+    if(!sound->offset){
+        return -1;
+    }
+
+    SPU_CH_VOL_L(ch) = left;
+	SPU_CH_VOL_R(ch) = right;
+	SPU_CH_FREQ (ch) = sound->sampleRate;
+	SPU_CH_ADDR (ch) = sound->offset / 8;
+	SPU_CH_ADSR1(ch) = 0x00ff;
+	SPU_CH_ADSR2(ch) = 0x0000;
+
+    if(ch < 16){
+        SPU_FLAG_ON1 = 1 << ch;
+    } else {
+        SPU_FLAG_ON2 = 1 << (ch - 16);
+    }
+
+    return ch;
+}
+
+/* Stream Class */
+
+/*
+ * The stream driver lays out a ring buffer of interleaved audio chunks in SPU
+ * RAM as follows:
+ *
+ * +---------------------------------+---------------------------------+-----
+ * |              Chunk              |              Chunk              |
+ * | +------------+------------+     | +------------+------------+     |
+ * | |  Ch0 data  |  Ch1 data  | ... | |  Ch0 data  |  Ch1 data  | ... | ...
+ * | +------------+------------+     | +------------+------------+     |
+ * +-^------------^------------------+-^------------^------------------+-----
+ *   | Ch0 start  | Ch1 start          | Ch0 loop   | Ch1 loop
+ *                                     | IRQ address
+ *
+ * The length of each chunk is given by the interleave size multiplied by the
+ * channel count. Each data block must be terminated with the loop end and
+ * sustain flags set in order to make the channels "jump" to the next chunk's
+ * blocks.
+ * - SpicyJpeg
+ */
+
+void stream_configureIRQ(Stream *stream){
+    uint16_t ctrlReg = SPU_CTRL;
+
+    // Disable the IRQ if an underrun occurs.
+    // TODO: handle this in a slightly better way
+    if(!stream->_bufferedChunks){
+        SPU_CTRL = ctrlReg & ~SPU_CTRL_IRQ_ENABLE;
+        return;
+    }
+
+    // Exit if the IRQ has been set up before and not yet acknowledged by
+    // handleInterrupt().
+    if(ctrlReg & SPU_CTRL_IRQ_ENABLE){
+        return;
+    }
+
+    ChannelMask tempMask = stream->_channelMask;
+    uint32_t chunkOffset = stream_getChunkOffset(stream, stream->_head);
+
+    SPU_IRQ_ADDR = chunkOffset / 8;
+    SPU_CTRL     = ctrlReg | SPU_CTRL_IRQ_ENABLE;
+
+    for(Channel ch = 0; tempMask; ch++, tempMask >>= 1){
+        if(!(tempMask & 1)){
+            continue;
+        }
+
+        SPU_CH_LOOP_ADDR(ch) = chunkOffset /8;
+        chunkOffset         += stream->interleave;
+    }
+}
+
+Stream *stream_create(void){
+    Stream stream = {
+        ._channelMask = 0,
+        .offset       = 0,
+        .interleave   = 0,
+        .numChunks    = 0,
+        .sampleRate   = 0,
+        .channels     = 0
+    };
+
+    stream_resetBuffer(&stream);
+    return &stream;
+}
+
+bool stream_initFromVAGHeader(Stream *stream, VAGHeader *vagHeader, uint32_t _offset, size_t _numChunks){
+    if(stream_isPlaying(stream)){
+        return false;
+    }
+    if(!vagHeader_validateInterleavedMagic(vagHeader)){
+        return false;
+    }
+
+    stream_resetBuffer(stream);
+
+    stream->offset     = _offset;
+    stream->interleave = vagHeader->interleave;
+    stream->numChunks  = _numChunks;
+    stream->sampleRate = vagHeader_getSPUSampleRate(vagHeader);
+    stream->channels   = vagHeader_getNumChannels(vagHeader);
+    return true;
+}
+ 
+ChannelMask stream_startWithChannelMask(Stream *stream, uint16_t left, uint16_t right, ChannelMask mask) {
+    if(stream_isPlaying(stream) || !stream->_bufferedChunks){
+        return 0;
+    }
+
+    mask &= ALL_CHANNELS;
+
+    ChannelMask tempMask = mask;
+    uint32_t chunkOffset = stream_getChunkOffset(stream, stream->_head);
+    int isRightCh        = 0;
+
+    for(Channel ch = 0; tempMask; ch++, tempMask >>= 1){
+        if(!(tempMask & 1)){
+            continue;
+        }
+    
+        // Assume each pair of channels is a stero pair. If the channel count is odd,
+        // assume the last channel is mono.
+
+        if(isRightCh) {
+            SPU_CH_VOL_L(ch) = 0;
+            SPU_CH_VOL_R(ch) = right;
+        } else if(tempMask != 1){
+            SPU_CH_VOL_L(ch) = left;
+            SPU_CH_VOL_R(ch) = 0;
+        } else {
+            SPU_CH_VOL_L(ch) = left;
+            SPU_CH_VOL_R(ch) = right;
+        }
+
+        SPU_CH_FREQ(ch)  = stream->sampleRate;
+        SPU_CH_ADDR(ch)  = chunkOffset / 8;
+        SPU_CH_ADSR1(ch) = 0x00ff;
+        SPU_CH_ADSR2(ch) = 0x0000;
+
+        chunkOffset += stream->interleave;
+        isRightCh   ^= 1;
+    }
+
+    stream->_channelMask = mask;
+    SPU_FLAG_ON1 = mask & 0xffff;
+    SPU_FLAG_ON2 = mask >> 16;
+
+    stream_handleInterrupt(stream);
+    return mask;
+}
+
+void stream_stop(Stream *stream){
+    if(!stream_isPlaying(stream)){
+        return;
+    }
+
+    // Disabling the IRQ is always required in order to acknowledge it.
+    SPU_CTRL &= ~SPU_CTRL_IRQ_ENABLE;
+
+    stream->_head = (stream->_head + 1) % stream->numChunks;
+    stream->_bufferedChunks--;
+    stream_configureIRQ(stream);
+}
+
+size_t stream_feed(Stream *stream, const void *data, size_t length){
+    disableInterrupts();
+
+    uintptr_t ptr = (uintptr_t)(data);
+    size_t chunkLength = stream_getChunkLength(stream);
+
+    length = min(length, stream_getFreeChunkCount(stream) * chunkLength);
+
+    for(int i = length; i >= (int)(chunkLength); i -= chunkLength){
+        upload(
+            stream_getChunkOffset(stream, stream->_tail), (const void *)(ptr),
+            chunkLength, true
+        );
+
+        ptr += chunkLength;
+        stream->_tail = (stream->_tail + 1) % stream->numChunks;
+        stream->_bufferedChunks++;
+    }
+
+    if(stream_isPlaying(stream)){
+        stream_configureIRQ(stream);
+    }
+
+    flushWriteQueue();
+    return length;
+}
+
+void stream_resetBuffer(Stream *stream){
+    stream->_head            = 0;
+    stream->_tail            = 0;
+    stream ->_bufferedChunks = 0;
 }
