6272ab78bf1e1afe05c967e86481ef660707b65e
Rhys Baker <bakerrhys05@gmail.com>
2024-10-01 03:52:38 +0800

    Play a sound effect on button press

--- src/core/main.c
+++ src/core/main.c
@@ -21,6 +21,8 @@ extern const uint8_t fontData[];
 extern const uint8_t fontPalette[];
 TextureInfo font;
 
+extern const uint8_t computer_keyboard_spacebarAudio[];
+
 // the X and Y of the buffer we are currently using.
 int bufferX = 0;
 int bufferY = 0;
@@ -31,55 +33,142 @@ uint32_t *ptr;
 DMAChain dmaChains[2];
 bool usingSecondFrame = false;
 
+int screenColor = 0xfa823c;
+
 void waitForVblank(){
-   while(!vblank){
-      // Do nothing
-   }
-   vblank = false;
+    while(!vblank){
+        // Do nothing
+    }
+    vblank = false;
 }
+
+
+#define DMA_CHUNK_SIZE 4
+#define DMA_TIMEOUT 100000
+#define STATUS_TIMEOUT 10000
+
+
+static bool waitForStatus(uint16_t mask, uint16_t value) {
+	for (int timeout = STATUS_TIMEOUT; timeout > 0; timeout -= 10) {
+		if ((SPU_STAT & mask) == value)
+			return true;
+
+		delayMicroseconds(10);
+	}
+
+	return false;
+}
+
+size_t uploadAudioSample(uint32_t ramOffset, const void *data, size_t length, bool wait) {
+    length /= 4;
+    length = (length + DMA_CHUNK_SIZE - 1) / DMA_CHUNK_SIZE;
+
+    if (!waitForDMATransfer(DMA_SPU, DMA_TIMEOUT))
+        return 0;
+    
+    uint16_t ctrlReg = SPU_CTRL & ~SPU_CTRL_XFER_BITMASK;
+
+    SPU_CTRL = ctrlReg;
+    waitForStatus(SPU_CTRL_XFER_BITMASK, 0);
+
+    SPU_DMA_CTRL = 4;
+    SPU_ADDR = ramOffset / 8;
+    SPU_CTRL = ctrlReg | SPU_CTRL_XFER_DMA_WRITE;
+    waitForStatus(SPU_CTRL_XFER_BITMASK, SPU_CTRL_XFER_DMA_WRITE);
+
+    DMA_MADR(DMA_SPU) = (uint32_t)data;
+    DMA_BCR (DMA_SPU) = DMA_CHUNK_SIZE | (length << 16);
+    DMA_CHCR(DMA_SPU) = DMA_CHCR_WRITE | DMA_CHCR_MODE_SLICE | DMA_CHCR_ENABLE;
+
+    if (wait)
+        waitForDMATransfer(DMA_SPU, DMA_TIMEOUT);
+    
+    return length * DMA_CHUNK_SIZE * 4;
+}
+
 
 // Gets called once at the start of main.
 void init(void){
-   // Initialise the serial port for printing
-   initSerialIO(115200);
-   initSPU();
-   initCDROM();
-   initIRQ();
-   initControllerBus();
+    // Initialise the serial port for printing
+    initSerialIO(115200);
+    //initSPU();
+    //initCDROM();
+    initIRQ();
+    initControllerBus();
 
-   // Read the GPU's status register to check if it was left in PAL or NTSC mode by the BIOS
-   if ((GPU_GP1 & GP1_STAT_MODE_BITMASK) == GP1_STAT_MODE_PAL){
-      setupGPU(GP1_MODE_PAL, SCREEN_WIDTH, SCREEN_HEIGHT);
-   } else {
-      setupGPU(GP1_MODE_NTSC, SCREEN_WIDTH, SCREEN_HEIGHT);
-   }
+    // Read the GPU's status register to check if it was left in PAL or NTSC mode by the BIOS
+    if ((GPU_GP1 & GP1_STAT_MODE_BITMASK) == GP1_STAT_MODE_PAL){
+       setupGPU(GP1_MODE_PAL, SCREEN_WIDTH, SCREEN_HEIGHT);
+    } else {
+       setupGPU(GP1_MODE_NTSC, SCREEN_WIDTH, SCREEN_HEIGHT);
+    }
 
-   // Enable the GPU's DMA channel
-   DMA_DPCR |= DMA_DPCR_ENABLE << (DMA_GPU * 4);
-   DMA_DPCR |= DMA_DPCR_ENABLE << (DMA_OTC * 4);
+    // Enable the GPU's DMA channel
+    DMA_DPCR |= DMA_DPCR_ENABLE << (DMA_GPU * 4);
+    DMA_DPCR |= DMA_DPCR_ENABLE << (DMA_OTC * 4);
 
-   GPU_GP1 = gp1_dmaRequestMode(GP1_DREQ_GP0_WRITE); // Fetch GP0 commands from DMA when possible
-   GPU_GP1 = gp1_dispBlank(false); // Disable display blanking
+    // Enable the SPU's DMA channel
+    DMA_DPCR |= DMA_DPCR_ENABLE << (DMA_SPU * 4);
 
-   // Upload textures
-   uploadIndexedTexture(&font, fontData, SCREEN_WIDTH+16, 0, FONT_WIDTH, FONT_HEIGHT, 
-      fontPalette, SCREEN_WIDTH+16, FONT_HEIGHT, GP0_COLOR_4BPP);
+
+    BIU_DEV4_CTRL = 0
+    | ( 1 << 0) // Write delay
+    | (14 << 4) // Read delay
+    | BIU_CTRL_RECOVERY
+    | BIU_CTRL_WIDTH_16
+    | BIU_CTRL_AUTO_INCR
+    | (9 << 16) // Number of address lines
+    | (0 << 24) // DMA read/write delay
+    | BIU_CTRL_DMA_DELAY;
+
+    // Disable SPU
+    SPU_CTRL = 0;
+
+    // Set Master Volume and Reverb volume
+    // 15-bit signed value with leading 0
+    SPU_MASTER_VOL_L = 0b0011111111111111;
+    SPU_MASTER_VOL_R = 0b0011111111111111;
+    // Turn off reverb
+    SPU_REVERB_VOL_L = 0;
+    SPU_REVERB_VOL_R = 0;
+
+    SPU_FLAG_REVERB1 = 0;
+    SPU_FLAG_REVERB2 = 0;
+
+    // Not using reverb so set it to this.
+    SPU_REVERB_ADDR = 0xfffe;
+
+    // Reset every voice channel
+    for(int i=0; i<24; i++){
+        SPU_CH_VOL_L(i) = 0;
+        SPU_CH_VOL_R(i) = 0;
+        SPU_CH_ADSR1(i) = 0x00ff;
+        SPU_CH_ADSR2(i) = 0x0000;
+    }
+    // Enable the SPU
+    SPU_CTRL = 0
+    | ( 1 << 15) // Enable
+    | ( 1 << 14) // Mute
+    ;
+    
+    // Release all channels by setting every KeyOff flag
+    SPU_FLAG_OFF1 = 0xFF;
+    SPU_FLAG_OFF2 = 0x0F;
+
+
+    GPU_GP1 = gp1_dmaRequestMode(GP1_DREQ_GP0_WRITE); // Fetch GP0 commands from DMA when possible
+    GPU_GP1 = gp1_dispBlank(false); // Disable display blanking
+
+
+    // Upload textures
+    uploadIndexedTexture(&font, fontData, SCREEN_WIDTH+16, 0, FONT_WIDTH, FONT_HEIGHT, 
+        fontPalette, SCREEN_WIDTH+16, FONT_HEIGHT, GP0_COLOR_4BPP);
+
+    if(!uploadAudioSample(0x01000, computer_keyboard_spacebarAudio+0x30, 0xB70, true)){
+        screenColor = 0x0000FF;
+    }
 }
 
-uint8_t isoHeader[2048];
-uint8_t pvdData[2048];
-
-//void hexdump(const uint8_t *ptr, size_t length) {
-//    while (length) {
-//        size_t lineLength = (length < 16) ? length : 16;
-//        length -= lineLength;
-//
-//        for (; lineLength; lineLength--)
-//            printf(" %02x", *(ptr++));
-//
-//        putchar('\n');
-//    }
-//}
 
 ControllerInfo controllerInfo;
 bool squarePressed = false;
@@ -89,175 +178,135 @@ bool l1Pressed     = false;
 bool upPressed     = false;
 bool downPressed   = false;
 
-int ReadSector = 16;
-int DisplayingPage = 0;
-char* UIBuffer[32];
-char* DataBuffer[2048];
-char* LoadingString = "Loading...";
 
-void formatDataOutput(char* string){
-   for(int i = 0; i<1024; i++){
-      if(!(i%40)){
-         sprintf(DataBuffer, "%s\n", DataBuffer);
-      }
-      sprintf(DataBuffer, "%s%c", DataBuffer, string[i + (1024 * DisplayingPage)]);
-   }
+void hexdump(const uint8_t *ptr, size_t length) {
+    while (length) {
+        size_t lineLength = (length < 16) ? length : 16;
+        length -= lineLength;
+
+        for (; lineLength; lineLength--)
+            printf(" %02x", *(ptr++));
+
+        putchar('\n');
+    }
 }
 
-uint8_t rootDirEntry[34];
-uint32_t rootDirLBA;
-uint8_t rootDirData[2048];
+
+// Start of main
 
 int main(void){   
-   // Tell the compiler that variables might be updated randomly (ie, IRQ handlers)
-   __atomic_signal_fence(__ATOMIC_ACQUIRE);
+    // Tell the compiler that variables might be updated randomly (ie, IRQ handlers)
+    __atomic_signal_fence(__ATOMIC_ACQUIRE);
 
-   // Initialise important things for later
-   init();
+    // Initialise important things for later
+    init();
 
-   //printf("\n\n==== PROGRAM START ====\n\n");
+    // Load song
+    
+    // Play song on loop on chanel 3
 
-   // Main loop. Runs every frame, forever
+
+
+    // Main loop. Runs every frame, forever
     for(;;){
-      // Point to the relevant DMA chain for this frame, then swap the active frame.
-      DMAChain *chain = &dmaChains[usingSecondFrame];
-      usingSecondFrame = !usingSecondFrame;
+        // Point to the relevant DMA chain for this frame, then swap the active frame.
+        DMAChain *chain = &dmaChains[usingSecondFrame];
+        usingSecondFrame = !usingSecondFrame;
 
-      // Reset the ordering table to a blank state.
-      clearOrderingTable((chain->orderingTable), ORDERING_TABLE_SIZE);
-      chain->nextPacket = chain->data;
-
-
-      sprintf(UIBuffer, "Read Sector: \t%d \tPage: %d/4", ReadSector, DisplayingPage+1);
-
-      printString(chain, &font, 10, 10, UIBuffer);
-      printDataString(chain, &font, 10, 20, DataBuffer);
+        // Reset the ordering table to a blank state.
+        clearOrderingTable((chain->orderingTable), ORDERING_TABLE_SIZE);
+        chain->nextPacket = chain->data;
       
-      // Place the framebuffer offset and screen clearing commands last.
-      // This means they will be executed first and be at the back of the screen.
-      ptr = allocatePacket(chain, ORDERING_TABLE_SIZE -1 , 3);
-      ptr[0] = gp0_rgb(60, 130, 250) | gp0_vramFill();
-      ptr[1] = gp0_xy(bufferX, bufferY);
-      ptr[2] = gp0_xy(SCREEN_WIDTH, SCREEN_HEIGHT);
+        // Place the framebuffer offset and screen clearing commands last.
+        // This means they will be executed first and be at the back of the screen.
+        ptr = allocatePacket(chain, ORDERING_TABLE_SIZE -1 , 3);
+        ptr[0] = screenColor | gp0_vramFill();
+        ptr[1] = gp0_xy(bufferX, bufferY);
+        ptr[2] = gp0_xy(SCREEN_WIDTH, SCREEN_HEIGHT);
 
-      ptr = allocatePacket(chain, ORDERING_TABLE_SIZE - 1, 4);
-      ptr[0] = gp0_texpage(0, true, false);
-      ptr[1] = gp0_fbOffset1(bufferX, bufferY);
-      ptr[2] = gp0_fbOffset2(bufferX + SCREEN_WIDTH - 1, bufferY + SCREEN_HEIGHT - 2);
-      ptr[3] = gp0_fbOrigin(bufferX, bufferY);
+        ptr = allocatePacket(chain, ORDERING_TABLE_SIZE - 1, 4);
+        ptr[0] = gp0_texpage(0, true, false);
+        ptr[1] = gp0_fbOffset1(bufferX, bufferY);
+        ptr[2] = gp0_fbOffset2(bufferX + SCREEN_WIDTH - 1, bufferY + SCREEN_HEIGHT - 2);
+        ptr[3] = gp0_fbOrigin(bufferX, bufferY);
       
       
-      getControllerInfo(0, &controllerInfo);
-      // Square
-      if(controllerInfo.buttons & BUTTON_MASK_SQUARE){
-         if(!squarePressed){
-            squarePressed = true;
-            //printf("\n\n==== READ CDROM DATA ====\n\n");
-            printString(chain, &font, 200, 10, LoadingString);
-            startCDROMRead(
-               ReadSector,
-               isoHeader,
-               sizeof(isoHeader) / 2048,
-               2048,
-               true
-            );
-            waitForINT1();
-            //hexdump(isoHeader, 2048);
-            sprintf(DataBuffer, "");
-            formatDataOutput(isoHeader);
-         }
-      } else {
-         squarePressed = false;
-      }
-      
+        getControllerInfo(0, &controllerInfo);
+        // Square
+        if(controllerInfo.buttons & BUTTON_MASK_SQUARE){
+            if(!squarePressed){
+                squarePressed = true;
+                SPU_CH_VOL_L(0) = 0b0011111111111111;
+                SPU_CH_VOL_R(0) = 0b0011111111111111;
+                SPU_CH_FREQ (0) = 1<<12;
+                SPU_CH_ADDR (0) = 0x1000 / 8;
+                SPU_CH_ADSR1(0) = 0x00ff;
+                SPU_CH_ADSR2(0) = 0x0000;
+
+                SPU_FLAG_ON1 = 1<<0;
+            }
+        } else {
+            squarePressed = false;
+        }
+
         // Circle
         if(controllerInfo.buttons & BUTTON_MASK_CIRCLE){
             if(!circlePressed){
                 circlePressed = true;
-                
-                getRootDirData(&rootDirData);
-                uint8_t directoryListingLength = 0;
-                DirectoryEntry *directoryListing[10];
 
-                DirectoryEntry directoryEntry;
-
-                uint8_t  recLen;
-                uint32_t len;
-                uint32_t lba;
-                char *name[255];
-                int offset = 0;
-                //printf("\n\n==== Directory Contents ====\n\n");
-                for(int i=0; i<10; i++){
-                    if(parseDirRecord(
-                        &rootDirData[offset],
-                        &recLen,
-                        directoryListing[i]
-                    )){
-                       break;
-                    }
-                    offset += recLen;
-                    //printf("%d: \"%s\" | %d\n", i, &directoryListing[i]->name, directoryListing[i]->lba);
-                }
             }
-      } else {
-         circlePressed = false;
-      }
+        } else {
+            circlePressed = false;
+        }
 
-      // R1
+        // R1
         if(controllerInfo.buttons & BUTTON_MASK_R1){
             if(!r1Pressed){
                 r1Pressed = true;
-                if(DisplayingPage++ >= 3) DisplayingPage = 0;
-                sprintf(DataBuffer, "");
-                formatDataOutput(isoHeader);
+
             }
-      } else {
-         r1Pressed = false;
-      }
+        } else {
+            r1Pressed = false;
+        }
 
-      // L1
-      if(controllerInfo.buttons & BUTTON_MASK_L1){
-         if(!l1Pressed){
-            l1Pressed = true;
-            if(DisplayingPage-- <= 0) DisplayingPage = 3;
-            sprintf(DataBuffer, "");
-            formatDataOutput(isoHeader);
-         }
-      } else {
-         l1Pressed = false;
-      }
+        // L1
+        if(controllerInfo.buttons & BUTTON_MASK_L1){
+            if(!l1Pressed){
+                l1Pressed = true;
+              
+            }
+        } else {
+            l1Pressed = false;
+        }
 
-      // Up
-      if(controllerInfo.buttons & BUTTON_MASK_UP){
-         if(!upPressed){
-            // Max number of sectors
-            if(ReadSector++ >= 214349) ReadSector = 0;
-            upPressed = true;
-         }
-      } else {
-         upPressed = false;
-      }
+        // Up
+        if(controllerInfo.buttons & BUTTON_MASK_UP){
+            if(!upPressed){
+                upPressed = true;
+            }
+        } else {
+            upPressed = false;
+        }
 
-      // Down
-      if(controllerInfo.buttons & BUTTON_MASK_DOWN){
-         if(!downPressed){
-            if(ReadSector-- <= 0) ReadSector = 214349;
-            downPressed = true;
-         }
-      } else {
-         downPressed = false;
-      }
+        // Down
+        if(controllerInfo.buttons & BUTTON_MASK_DOWN){
+            if(!downPressed){
+                downPressed = true;
+            }
+        } else {
+            downPressed = false;
+        }
 
-      waitForGP0Ready();
-      waitForVblank();
+        waitForGP0Ready();
+        waitForVblank();
 
-      // Swap the frame buffers.
-      bufferY = usingSecondFrame ? SCREEN_HEIGHT : 0;
-      GPU_GP1 = gp1_fbOffset(bufferX, bufferY); 
+        // Swap the frame buffers.
+        bufferY = usingSecondFrame ? SCREEN_HEIGHT : 0;
+        GPU_GP1 = gp1_fbOffset(bufferX, bufferY); 
 
-      // Give DMA a pointer to the last item in the ordering table.
-      // We don't need to add a terminator, as it is already done for us by the OTC.
-      sendLinkedList(&(chain->orderingTable)[ORDERING_TABLE_SIZE - 1]);
+        // Give DMA a pointer to the last item in the ordering table.
+        // We don't need to add a terminator, as it is already done for us by the OTC.
+        sendLinkedList(&(chain->orderingTable)[ORDERING_TABLE_SIZE - 1]);
    }
 
 
