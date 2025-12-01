c340792fc4d8580b4eac1d7e668e9c032cd0f6d5
Rhys-Baker <bakerrhys05@gmail.com>
2025-12-02 01:05:17 +0800

    Add more robust FileStream code to filesystem library

--- include/core/filesystem.h
+++ include/core/filesystem.h
@@ -20,6 +20,14 @@ typedef struct{
    char name[255];
 } DirectoryEntry;
 
+typedef struct FileStream {
+   uint8_t buffer[2048];  // Buffer to store the current sector
+   uint32_t globalOffset; // absolute file offset
+   uint32_t fileLBA;      // Starting LBA on disc
+   uint32_t sectorIndex;  // Currently loaded sector
+} FileStream;
+
+
 // Functions
 
 void initFilesystem(void);
@@ -27,3 +35,33 @@ uint32_t getRootDirLba(uint8_t *pvdSector, uint32_t *LBA);
 int parseDirRecord(uint8_t *dataSector, uint8_t *recordLength, DirectoryEntry *directoryEntry);
 void getRootDirData(void *rootDirData);
 uint32_t getLbaToFile(const char *filename, uint32_t fileSize[2]);
+
+
+/// @brief Create a FileStream to a given filename
+/// @param filename Name of the file to open
+/// @return FileStream or NULL if not found
+FileStream *FileStream_open(const char *filename);
+/// @brief Close/Free a FileStream
+/// @param s Pointer to FileStream to close
+void        FileStream_close(FileStream *s);
+/// @brief Seek to a given offset
+/// @param s Pointer to FileStream
+/// @param offset Offset from start of file
+void        FileStream_seek(FileStream *s, uint32_t offset);
+/// @brief Read a 1-byte uint8_t from the file and increment the global offset
+/// @param s Pointer to FileStream
+/// @return The value that was read
+uint8_t     FileStream_read8(FileStream *s);
+/// @brief Read a 2-byte uint16_t from the file and increment the global offset
+/// @param s Pointer to FileStream
+/// @return The value that was read
+uint16_t    FileStream_read16(FileStream *s);
+/// @brief Read a 4-byte uint32_t from the file and increment the global offset
+/// @param s Pointer to FileStream
+/// @return The value that was read
+uint32_t    FileStream_read32(FileStream *s);
+/// @brief Read n bytes into dest from FileStream
+/// @param s Pointer to Filestream
+/// @param dest Destination buffer to write data to
+/// @param n number of bytes to read
+void FileStream_readBytes(FileStream *s, void *dest, uint32_t n);
--- src/core/filesystem.c
+++ src/core/filesystem.c
@@ -1,5 +1,6 @@
 #include "filesystem.h"
 #include "cdrom.h"
+#include <stdlib.h>
 
 // Internal global variable for this lib. Hides away the rootDirData for internal use.
 uint8_t rootDirData[2048];
@@ -90,6 +91,7 @@ void getRootDirData(void *rootDirData){
 /// @brief Get the LBA to the file with a given filename, assuming it is stored in the root directory.
 /// @param rootDirData Pointer to the root directory data.
 /// @param filename String containing the filename of the requested file.
+/// @param fileSize Pointer to the first of two numbers in an array. Basically a 64-bit number
 /// @return LBA to file or 0 if not found.
 uint32_t getLbaToFile(const char *filename, uint32_t fileSize[2]){
     DirectoryEntry directoryEntry;
@@ -106,10 +108,103 @@ uint32_t getLbaToFile(const char *filename, uint32_t fileSize[2]){
         offset += recLen;
 
         if(!__builtin_strcmp(directoryEntry.name, filename)){
-            fileSize[0] = directoryEntry.fileSize[0];
-            fileSize[1] = directoryEntry.fileSize[1];
+            if(fileSize != NULL){
+                fileSize[0] = directoryEntry.fileSize[0];
+                fileSize[1] = directoryEntry.fileSize[1];
+            }
             return directoryEntry.lba;
         }
     }
     return 0;
+}
+
+
+
+// FileStream functions
+
+FileStream *FileStream_open(const char *filename){
+    if(!filename) return NULL;
+    FileStream *s;
+    uint32_t lba = getLbaToFile(filename, NULL);
+    if(lba == 0){
+        return NULL;
+    }
+    s = malloc(sizeof(FileStream));
+    s->fileLBA = lba;
+    s->sectorIndex = 0;
+    startCDROMRead(
+        s->fileLBA + s->sectorIndex,
+        s->buffer,
+        1, 2048,
+        true, true
+    );
+    return s;
+}
+void FileStream_close(FileStream *s){
+    if(!s) return;
+    free(s);
+}
+void FileStream_seek(FileStream *s, uint32_t offset){
+    if(!s) return;
+    s->globalOffset = offset;
+    uint32_t expectedSector = offset >> 11;
+
+    // If the sector we want to read is not currently loaded, load the required sector
+    if (expectedSector != s->sectorIndex){
+        s->sectorIndex = expectedSector;
+        startCDROMRead(
+            s->fileLBA + s->sectorIndex,
+            s->buffer,
+            1, 2048,
+            true, true
+        );
+    }
+}
+uint8_t FileStream_read8(FileStream *s){
+    if(!s) return 0;
+    uint32_t sectorOffset = s->globalOffset & 2047;  // % 2048
+    uint32_t expectedSector = s->globalOffset >> 11; // / 2048 
+
+    // If the sector we want to read is not currently loaded, load the required sector
+    if (expectedSector != s->sectorIndex){
+        s->sectorIndex = expectedSector;
+        startCDROMRead(
+            s->fileLBA + s->sectorIndex,
+            s->buffer,
+            1, 2048,
+            true, true
+        );
+    }
+
+    // Read the byte we want
+    uint8_t v = s->buffer[sectorOffset];
+    s->globalOffset++;
+    return v;
+}
+uint16_t FileStream_read16(FileStream *s){
+    if(!s) return 0;
+    uint16_t v;
+    v = (
+        FileStream_read8(s) |
+        FileStream_read8(s) << 8
+    );
+    return v;
+}
+uint32_t FileStream_read32(FileStream *s){
+    if(!s) return 0;
+    uint32_t v;
+    v = (
+        FileStream_read8(s)       |
+        FileStream_read8(s) << 8  |
+        FileStream_read8(s) << 16 |
+        FileStream_read8(s) << 24
+    );
+    return v;
+}
+void FileStream_readBytes(FileStream *s, void *dest, uint32_t n){
+    if(!s) return;
+    if(!dest) return;
+    for(int i=0; i<n; i++){
+        ((uint8_t*)dest)[i] = FileStream_read8(s);
+    }
 }
